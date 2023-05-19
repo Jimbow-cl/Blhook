d094aba77374cc1dbf67ab42c39491f00c22e0c5
Celine CC <celine.c11@outlook.com>
2023-05-19 10:32:45 +0200

    branche

--- src/layout/Menu.jsx
+++ src/layout/Menu.jsx
@@ -3,8 +3,6 @@ function Menu() {
 
     return (
         <nav>
-
-            <Link to="/"><button>Home</button></Link>
             <Link to="/Login"><button>Vide1</button></Link>
 
         </nav>
