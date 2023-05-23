50632432ce9a67a950fa90a0ad37edf85404f0b1
Jimbow-cl <cart-lamy.jimmy@live.fr>
2023-05-23 12:25:33 +0200

    commit filter post 1

--- src/components/post/ConnectPost.jsx
+++ src/components/post/ConnectPost.jsx
@@ -1,5 +1,52 @@
+import { useState, useEffect } from "react"
+import TokenStorage from "../Connect/StorageToken"
+import ModelsPost from "../../models/ModelsPost";
+import moment from "moment";
+import 'moment/dist/locale/fr';
+import 'moment/locale/fr';
+
+
+// Conposant de tri des postes d'un utilisateur
+
 function ConnectPost() {
 
+    const [connect, setConnect] = useState();
+    useEffect(() => {
+        TokenStorage(setConnect)
+    }, []);
+
+    const [post, setPost] = useState([]);
+    const [filterPost, setFilterPost] = useState([]);
+
+
+    async function Post1() {
+        let response = await fetch(`https://social-network-api.osc-fr1.scalingo.io/theBlhook/posts?page=0&limit=20`);
+        console.log('reponse post', response);
+        let donnees = await response.json();
+        console.log('données post', donnees.posts);
+        setPost(donnees.posts);
+
+    }
+    useEffect(() => { Post1() }, []);
+
+    const RenderMyArray = () => {
+
+        return post.map((item, id) => {
+
+            return (
+                <div>
+                    <ModelsPost key={id} title={item.title} post={item.content} lastname={item.lastname} date={moment(item.date).format("LL")} />
+                </div>
+            );
+        }
+        );
+
+
+    };
+    return (
+        <div className='d-flex align'> <RenderMyArray /></div>
+
+    );
 
 }
 
